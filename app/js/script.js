c32305a70f2f07f61ed8ccfd5f2bac5689467ace
Piotr Brzezinski <piotrbrzezinski@clm23-poz-pb.home>
2021-03-14 20:25:13 +0100

    menu mobilen

---
+++ app/js/script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+const menuItems = document.querySelector('.mobile');
+const items = document.querySelectorAll('.nav .mobile .menu .menu__items a');
+const social = document.querySelectorAll('.nav .mobile .menu .menu__social .row');
+
+burger.addEventListener("click", () => {
+    menuItems.classList.toggle("open");
+    items.forEach(link=>{
+        link.classList.toggle("fade");
+    social.forEach(row=>{
+        row.classList.toggle("fade");
+        })
+    });
+
+})

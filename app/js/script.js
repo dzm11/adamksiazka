f8840e1372274ec2b61e05efe5e9a5c91988ec0a
Piotr Brzezinski <piotrbrzezinski@clm23-poz-pb.home>
2021-03-14 22:15:18 +0100

    poprawki w responsywnosci guruny i menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector('.burger');
+const body = document.querySelector('body');
 const menuItems = document.querySelector('.mobile');
 const items = document.querySelectorAll('.nav .mobile .menu .menu__items a');
-const social = document.querySelectorAll('.nav .mobile .menu .menu__social .row');
+const social = document.querySelectorAll('.nav .mobile .menu__social .row');
 
 burger.addEventListener("click", () => {
+    body.classList.toggle("open");
     menuItems.classList.toggle("open");
     items.forEach(link=>{
         link.classList.toggle("fade");
